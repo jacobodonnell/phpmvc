442f934c9ea942c792cd1d3d6e24b0c80043f6b6
Jacob O'Donnell <jacobodonnell96@proton.me>
2025-06-27 21:33:29 -0500

    complete setting up router variables

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 define('BASE_PATH', dirname(__DIR__));
 
-$path = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 spl_autoload_register(function (string $class_name) {
     require BASE_PATH . '/src/' . str_replace('\\', '/', $class_name) . '.php';
@@ -10,9 +10,15 @@ spl_autoload_register(function (string $class_name) {
 
 $router = new Framework\Router;
 
+$router->add(
+    "/product/{slug:[\w-]+}",
+    ['controller' => 'products', 'action' => 'show']
+);
+$router->add("/{controller}/{id:\d+}/{action}");
 $router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
 $router->add('/products', ['controller' => 'products', 'action' => 'index']);
 $router->add('/', ['controller' => 'home', 'action' => 'index']);
+$router->add("/{controller}/{action}");
 
 $params = $router->match($path);
 
